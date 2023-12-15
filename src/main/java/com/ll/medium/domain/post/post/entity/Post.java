beb2952f293b74dd5faaa82787ff9c0eb36531ed
jhs512 <jangka512@gmail.com>
2023-12-15 17:22:30 +0900

    step 10

--- src/main/java/com/ll/medium/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/medium/domain/post/post/entity/Post.java
@@ -1,12 +1,18 @@
 package com.ll.medium.domain.post.post.entity;
 
 import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.post.postLike.entity.PostLike;
 import com.ll.medium.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -16,6 +22,10 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 @Setter
 public class Post extends BaseEntity {
+    @OneToMany(mappedBy = "post", cascade = ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<PostLike> likes = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Member author;
     private String title;
@@ -26,5 +36,25 @@ public class Post extends BaseEntity {
 
     public void increaseHit() {
         hit++;
+    }
+
+    public void like(Member member) {
+        if (hasLike(member)) {
+            return;
+        }
+
+        likes.add(PostLike.builder()
+                .post(this)
+                .member(member)
+                .build());
+    }
+
+    public boolean hasLike(Member member) {
+        return likes.stream()
+                .anyMatch(postLike -> postLike.getMember().equals(member));
+    }
+
+    public void cancelLike(Member member) {
+        likes.removeIf(postLike -> postLike.getMember().equals(member));
     }
 }
