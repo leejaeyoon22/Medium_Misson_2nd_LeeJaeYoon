d0c1c9d1f473666735646c65331c1533645b3fc1
jhs512 <jangka512@gmail.com>
2023-12-15 14:44:10 +0900

    step 1

--- src/main/java/com/ll/medium/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/medium/domain/post/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.ll.medium.domain.post.post.repository;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.post.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,4 +18,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             String kw_,
             Pageable pageable
     );
+
+    Page<Post> findByAuthorAndTitleContainingIgnoreCaseOrAuthorAndBodyContainingIgnoreCase(Member author, String kw, Member author_, String kw_, Pageable pageable);
 }
